06a9d18fab9ce5529d0cded0ad6f846c010f1a5b
MatthewZoltak <matthew.zoltak@mail.mcgill.ca>
2019-03-18 21:34:48 -0400

    user info added

--- DietPlanners-web/src/components/MyAccount.vue
+++ DietPlanners-web/src/components/MyAccount.vue
@@ -6,7 +6,38 @@
       <router-link to="/" id="logout">Logout</router-link>
     </div>
     <h1>{{ msg }}</h1>
-
+    <div class="account-page">
+    <form @submit.prevent="handleSubmit">
+            <div class="username">
+                <label for="username">Username</label>
+                <input type="text" onkeypress="this.style.width = ((this.value.length) * 8) + 'px';" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
+                <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
+            </div>
+            <div class="height">
+                <label htmlFor="height">Height</label>
+                <input type="height" onkeypress="this.style.width = ((this.value.length ) * 8) + 'px';" v-model="height" name="height" class="form-control" :class="{ 'is-invalid': submitted && !height }" />
+                <div v-show="submitted && !height" class="invalid-feedback">Height is required</div>
+            </div>
+            <div class="targetweight">
+                <label htmlFor="targetweight">Target Weight</label>
+                <input type="targetweight" onkeypress="this.style.width = ((this.value.length ) * 8) + 'px';" v-model="targetweight" name="targetweight" class="form-control" :class="{ 'is-invalid': submitted && !targetweight }" />
+                <div v-show="submitted && !targetweight" class="invalid-feedback">Target weight is required</div>
+            </div>
+            <div class="targetdate">
+                <label htmlFor="targetdate">Target Date</label>
+                <input type="targetdate" onkeypress="this.style.width = ((this.value.length ) * 8) + 'px';" v-model="targetdate" name="targetdate" class="form-control" :class="{ 'is-invalid': submitted && !targetdate }" />
+                <div v-show="submitted && !targetdate" class="invalid-feedback">Target date is required</div>
+            </div>
+            <div class="startweight">
+                <label htmlFor="startweight">Start Weight</label>
+                <input type="startweight" onkeypress="this.style.width = ((this.value.length ) * 8) + 'px';" v-model="startweight" name="startweight" class="form-control" :class="{ 'is-invalid': submitted && !startweight }" />
+                <div v-show="submitted && !startweight" class="invalid-feedback">Start weight is required</div>
+            </div>
+            <div class="submit">
+                <button v-on:click="submitinfo" style="position: relative ;  margin-top: 25px;">Submit</button>
+            </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -22,6 +53,10 @@ export default {
 </script>
 
 <style scoped>
+
+  body {
+    margin-top: 10px;
+  }
  /* The sidebar menu */
   .sidenav {
     height: 100%; /* Full-height: remove this if you want "auto" height */
@@ -50,10 +85,21 @@ export default {
     color: black;
   }
 
+<<<<<<< HEAD
+  .account-page {
+    /* background-color: blue; */
+    height: 50vh; 
+    width: 50vw;
+    margin: 0 auto;
+
+  /* color: white; */
+}
+=======
   #logout {
     position: absolute;
     bottom: 1%;
     left: 21%;
   }
+>>>>>>> 05d2bd00e4420ed33b469a608031f48214d356e7
 
 </style>
